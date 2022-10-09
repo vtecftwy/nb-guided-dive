1d058afffe5d8df59be46536c448c082553f4eac
vtecftwy <github@procurasia.com>
2022-10-09 21:43:49 +0800

    refactor imports

--- nb_guided_dive/_modidx.py
+++ nb_guided_dive/_modidx.py
@@ -6,5 +6,4 @@ d = { 'settings': { 'branch': 'master',
                 'git_url': 'https://github.com/vtecftwy/nb-guided-dive',
                 'lib_path': 'nb_guided_dive'},
   'syms': { 'nb_guided_dive.core': {'nb_guided_dive.core.foo': ('core.html#foo', 'nb_guided_dive/core.py')},
-            'nb_guided_dive.imports': { 'nb_guided_dive.imports.gv': ('imports.html#gv', 'nb_guided_dive/imports.py'),
-                                        'nb_guided_dive.imports.mount_gdrive': ('imports.html#mount_gdrive', 'nb_guided_dive/imports.py')}}}
+            'nb_guided_dive.imports': {'nb_guided_dive.imports.gv': ('imports.html#gv', 'nb_guided_dive/imports.py')}}}
